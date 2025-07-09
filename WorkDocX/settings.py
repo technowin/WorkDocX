aa1b66a6418c2199744994bec8b99df07caee7bb
technowin <technowinitinfra@gmail.com>
2025-07-09 11:49:21 +0530

    fdfdfd

--- WorkDocX/settings.py
+++ WorkDocX/settings.py
@@ -27,8 +27,8 @@ ALLOWED_HOSTS = ['15.207.169.98']
 # ALLOWED_HOSTS = ['127.0.0.1']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-# DEBUG = True   
+# DEBUG = False
+DEBUG = True   
 
 import mimetypes
 mimetypes.add_type("application/javascript", ".mjs")
